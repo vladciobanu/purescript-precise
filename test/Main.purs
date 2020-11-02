a30a4248b2f6d592e2135d92973fa279cc80a7e7
Vladimir Ciobanu <admin@cvlad.info>
2020-11-02 16:36:46 +0200

    Fix HugeNum comparsion function.

--- test/Main.purs
+++ test/Main.purs
@@ -2,13 +2,16 @@ module Test.Main where
 
 import Prelude
 
-import Data.HugeNum (HugeNum)
-import Data.HugeNum.Gen (genHugeNum)
 import Data.HugeInt (HugeInt)
+import Data.HugeInt as HI
 import Data.HugeInt.Gen (genHugeInt)
+import Data.HugeNum (HugeNum)
+import Data.HugeNum as HN
+import Data.HugeNum.Gen (genHugeNum)
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
-import Test.QuickCheck (class Arbitrary)
+import Test.QuickCheck (class Arbitrary, quickCheck)
 import Test.QuickCheck.Laws.Data.Eq (checkEq)
 import Test.QuickCheck.Laws.Data.Ord (checkOrd)
 import Test.QuickCheck.Laws.Data.Ring (checkRing)
@@ -55,3 +58,13 @@ main = do
   checkOrd prxHugeInt
   checkSemiring prxHugeInt
   checkRing prxHugeInt
+  log "Checking from/to Num...\n"
+  quickCheck toFromNumIsIdentity
+  log "Checking from/to Int...\n"
+  quickCheck toFromIntIsIdentity
+
+toFromNumIsIdentity :: Number -> Boolean
+toFromNumIsIdentity n = HN.toNumber (HN.fromNumber n) == n
+
+toFromIntIsIdentity :: Int -> Boolean
+toFromIntIsIdentity n = HI.toInt (HI.fromInt n) == Just n
